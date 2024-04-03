505b417eb3320812305d6395d077e2c30c156547
mohamedsherifGharieb <112889370+mohamedsherifGharieb@users.noreply.github.com>
2024-04-03 06:30:21 +0200

    Committing changes before pushing to remote repository

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -71,7 +71,7 @@ public class Main extends Application {
     TextField userNameSField = new TextField();
     PasswordField passwordSField = new PasswordField();
     final String local = "http://localhost:3000";
-    final String heroku = "https://safe-reaches-92386-edf83a4939bb.herokuapp.com/";
+    final String heroku = "https://server---app-d244e2f2d7c9.herokuapp.com";
     String urlFinal = heroku; //local //heroku
     public void start(Stage primaryStage) throws Exception {
         //setting buttons effect
